026c08ade2dda20598a3412481eff5a298f84714
alice <tang.alice.000@gmail.com>
2023-02-26 17:17:19 -0800

    cors

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const cors = require('cors');
 const express = require('express');
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  methods: 'GET,PUT,POST,DELETE',
-  allowedHeaders: 'Content-Type',
-}));
+// Allow all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Set this header to 'true' only
+  next();
+});
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
-header('Access-Control-Allow-Headers: Content-Type');
+// Handle requests
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
+// Start server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
